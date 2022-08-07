d71357fac5e3bdc043aa77b962819df5bcbdcb04
Stephen Campbell <sjcr@mit.edu>
2022-08-06 23:28:39 -0400

    Setting up react w/ create-react-app

---
+++ interactive-card/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p> Test 1234</p>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="/">Stephen Campbell</a>.
+    </div>
+    </div>
+  );
+}
+
+export default App;
